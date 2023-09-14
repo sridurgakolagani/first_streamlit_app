e215a1f487c385e1d66bfa703d91496e5e3da264
sridurgakolagani <98544809+sridurgakolagani@users.noreply.github.com>
2023-09-14 18:38:51 -0500

    Update streamlit_app.py
    added try-except in fruityvice code

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,20 +23,19 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
+#new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-# display json version data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display data in table format
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
+except URLError as e:
+    streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
